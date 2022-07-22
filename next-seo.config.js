03b823ad26eefebd51014bd5a2c333dfd476620c
iseekTo <1214046231@qq.com>
2022-07-22 15:34:55 +0800

    feat(seo): add some config for seo

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+const SeoConfig = {
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    // url: "https://www.url.ie/",
+    site_name: "SiteName",
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
+
+export default SeoConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import { DefaultSeo } from "next-seo";
+
+import SeoConfig from "../next-seo.config";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...SeoConfig} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
